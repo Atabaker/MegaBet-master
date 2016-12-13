1ad79ea3aea3852e50ac214368a951c9f30fee33
jette <jkleemann@fh-bielefeld.de>
2016-12-13 17:35:31 +0100

    13.Dez 2.Version

--- app/src/main/java/de/fh_bielefeld/megabet/WetteAbgebenActivity.java
+++ app/src/main/java/de/fh_bielefeld/megabet/WetteAbgebenActivity.java
@@ -4,13 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import static de.fh_bielefeld.megabet.R.id.textViewTaler;
 
 public class WetteAbgebenActivity extends AppCompatActivity {
+
+    static User eingeloggertUser;
+
+    String user_taler = Double.toString(LoginActivity.getEingeloggertUser().getTaler());
+
+    private TextView textViewUser;
+    private TextView textViewTaler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wette_abgeben);
+
+        eingeloggertUser = LoginActivity.getEingeloggertUser();
+
+        loadData();
+    }
+    private void loadData(){
+        textViewUser = (TextView) findViewById(R.id.wette_textViewUsername);
+        textViewTaler = (TextView) findViewById(R.id.wette_TextViewTaler);
+        // findview for all views
+
+
+
+        textViewUser.setText(LoginActivity.getEingeloggertUser().getUsername());
+        textViewTaler.setText(user_taler);
+        //set text
     }
 
 
